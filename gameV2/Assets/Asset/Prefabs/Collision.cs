e84bf4dd8e5d3128f77e6d92d1186c578162f312
ahusba <ajhusband@gmail.com>
2014-07-01 11:52:22 +0100

    Version 2.0
    Clean repo of game jam.

---
+++ gameV2/Assets/Asset/Prefabs/Collision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+	public Collider physicsCollider;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+ 	void OnTriggerEnter(Collider other){	
+		Debug.Log(other.gameObject.name);
+		QuadrantActivate qa = (QuadrantActivate)other.gameObject.GetComponent<QuadrantActivate> ();
+		if (qa != null) {
+			qa.Activate();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
